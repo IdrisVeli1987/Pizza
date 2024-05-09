897bbee46171993303d9c1216659fecfc52bf4d0
IdrisVeli1987 <idrisveliev@gmail.com>
2024-05-09 21:07:59 +0400

    add classNames

---
+++ src/ui/Button/ui/Button.jsx
@@ -0,0 +1,29 @@
+import classNames from "classnames";
+import cls from "./Button.module.scss";
+
+const variantClasses = {
+  clear: "clear",
+  normal: "normal",
+};
+
+const Button = (props) => {
+  const { children, className, variant, active, border, onClick } = props;
+
+  const variantClass = variantClasses[variant] || variantClasses.normal;
+
+  console.log(variantClass);
+
+  return (
+    <div
+      className={classNames(`button ${className}`, cls[variantClass], {
+        [cls["active"]]: active,
+        [cls["border"]]: border,
+      })}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { Button };
