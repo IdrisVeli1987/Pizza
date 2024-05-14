aa39b50655087e007db5dcc1dc82fbe4f13e3909
IdrisVeli1987 <idrisveliev@gmail.com>
2024-05-14 15:49:03 +0400

    add Footer component to MainLayout

--- src/layouts/MainLayout/ui/MainLayout.jsx
+++ src/layouts/MainLayout/ui/MainLayout.jsx
@@ -2,13 +2,13 @@ import { Outlet } from "react-router-dom";
 import cls from "./MainLayout.module.scss";
 import { Header } from "@/Components/Header";
 import { useTheme } from "@/hooks/useTheme";
+import { Footer } from "@/Components/Footer";
 
 const MainLayout = () => {
   const { theme } = useTheme();
   return (
     <div id="app" className={`app ${theme}`}>
       <Header />
-
       <main className={cls.main}>
         <div className={cls.container}>
           <div className={cls.body}>
@@ -18,8 +18,7 @@ const MainLayout = () => {
           </div>
         </div>
       </main>
-
-      <p>Footer</p>
+      <Footer />{" "}
     </div>
   );
 };
