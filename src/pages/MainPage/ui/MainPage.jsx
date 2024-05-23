9993494d0c297aab71c8332d20bfd9f8b4b4fd4a
IdrisVeli1987 <idrisveliev@gmail.com>
2024-05-23 20:56:10 +0400

    add some code

--- src/pages/MainPage/ui/MainPage.jsx
+++ src/pages/MainPage/ui/MainPage.jsx
@@ -1,5 +1,47 @@
+import { ProductTape } from "@/Components/ProductTape";
+import { useGetOthers, useGetPizzas, useGetRolls } from "@/api/rtkApi";
+
 const MainPage = () => {
-  return <p>MainPage</p>;
+  const {
+    data: productsPizzas,
+    isLoading: pizzasLoading,
+    error: pizzasError,
+  } = useGetPizzas();
+
+  const {
+    data: productsRolls,
+    isLoading: rollsLoading,
+    error: rollsError,
+  } = useGetRolls();
+
+  const {
+    data: productsOthers,
+    isLoading: OthersLoading,
+    error: OthersError,
+  } = useGetOthers();
+
+  return (
+    <>
+      <ProductTape
+        title="Pizzalar"
+        products={productsPizzas}
+        isLoading={pizzasLoading}
+        error={pizzasError}
+      />
+      <ProductTape
+        title="Suşilər"
+        products={productsRolls}
+        isLoading={rollsLoading}
+        error={rollsError}
+      />
+      <ProductTape
+        title="İçkilər"
+        products={productsOthers}
+        isLoading={OthersLoading}
+        error={OthersError}
+      />
+    </>
+  );
 };
 
 export default MainPage;
