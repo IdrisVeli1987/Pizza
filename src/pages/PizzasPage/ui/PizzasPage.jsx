b92afa17bafe4c6de9361526c9bf4d29d1e80e6a
IdrisVeli1987 <idrisveliev@gmail.com>
2024-05-25 10:45:59 +0400

    create baseurl

--- src/pages/PizzasPage/ui/PizzasPage.jsx
+++ src/pages/PizzasPage/ui/PizzasPage.jsx
@@ -1,7 +1,26 @@
-const PizzasPage = () => {
-  return (
-    <p>PizzasPage</p>
-  )
-}
+import {
+  getPizzas,
+  getPizzasErrors,
+  getPizzasLoading,
+} from "@/redux/pizzas/selectors/pizzasSelectors";
+import { fetchPizzas } from "@/redux/pizzas/services/fetchPizzas";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-export default PizzasPage
+const PizzasPage = () => {
+  const pizzas = useSelector(getPizzas);
+  const error = useSelector(getPizzasErrors);
+  const loading = useSelector(getPizzasLoading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPizzas());
+  }, [dispatch]);
+
+  console.log("pizzas", pizzas);
+
+  return <p>PizzasPage</p>;
+};
+
+export default PizzasPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 import { rtkApi } from "@/api/rtkApi";
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./counter/slice";
+import { pizzasReducer } from "./pizzas/slice/pizzasSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    pizzas: pizzasReducer,
     [rtkApi.reducerPath]: rtkApi.reducer,
   },
-  
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(rtkApi.middleware),
 });
