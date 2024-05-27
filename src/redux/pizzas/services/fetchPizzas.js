3187e59c7993136bed2dd8ef495c9c9c064f4da8
IdrisVeli1987 <idrisveliev@gmail.com>
2024-05-27 17:28:36 +0400

    change rejectWithValue

--- src/redux/pizzas/services/fetchPizzas.js
+++ src/redux/pizzas/services/fetchPizzas.js
@@ -11,7 +11,7 @@ export const fetchPizzas = createAsyncThunk(
       return response.data;
        
     } catch (error) {
-      return rejectWithValue(error.message);
+      return rejectWithValue('Pizzalar tapılmadı');
     }
   }
 );
