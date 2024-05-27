68379eadf33bcfc4c07ebd9c5a8126169afb711c
IdrisVeli1987 <idrisveliev@gmail.com>
2024-05-27 17:28:05 +0400

    add calcMinPricePizzas

--- src/pages/PizzasPage/ui/PizzasPage.jsx
+++ src/pages/PizzasPage/ui/PizzasPage.jsx
@@ -1,9 +1,12 @@
+import { CartItem } from "@/Components/CartItem";
+import { ProductLayout } from "@/layouts/ProductLayout";
 import {
   getPizzas,
   getPizzasErrors,
   getPizzasLoading,
 } from "@/redux/pizzas/selectors/pizzasSelectors";
 import { fetchPizzas } from "@/redux/pizzas/services/fetchPizzas";
+import { calcMinPricePizzas } from "@/utils/calcMinPrice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -18,9 +21,26 @@ const PizzasPage = () => {
     dispatch(fetchPizzas());
   }, [dispatch]);
 
-  console.log("pizzas", pizzas);
+  if (error) {
+    return <div>{error}</div>;
+  }
 
-  return <p>PizzasPage</p>;
+  const item = pizzas.map((el) => {
+    const minPrice = calcMinPricePizzas(el.sizes, el.doughs);
+    return (
+      <CartItem
+        id={el.id}
+        key={el.id}
+        product={el.product}
+        img={el.photo}
+        title={el.name}
+        ingredients={el.ingredients}
+        price={minPrice}
+      />
+    );
+  });
+
+  return <ProductLayout header={"Pizzalar"} item={item} />;
 };
 
 export default PizzasPage;
