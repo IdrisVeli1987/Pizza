a060ca51aeb24920c559dd8eda483b58647b077b
IdrisVeli1987 <idrisveliev@gmail.com>
2024-06-07 18:57:25 +0400

    add ModalItemLayout component

---
+++ src/layouts/ModalItemLayout/ui/ModalItemLayout.jsx
@@ -0,0 +1,27 @@
+import { Button } from "@/ui/Button";
+import cls from "./ModalItemLayout.module.scss";
+
+const ModalItemLayout = (props) => {
+  const { params, options } = props;
+  return (
+    <div className={cls.body}>
+      <img className={cls.img} src={params.img} alt="" />
+
+      <div className={cls.content}>
+        <h3 className={cls.title}>{params.title}</h3>
+
+        {options && options}
+
+        <div className={cls.footer}>
+          <span className={cls.price}>Cəmi: {params.price} AZN</span>
+
+          <Button border className={cls.button}>
+            Əlavə et
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { ModalItemLayout };
