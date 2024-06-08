afa593e2d90d1e9cc352d50e67cbf83c8bf4081c
IdrisVeli1987 <idrisveliev@gmail.com>
2024-06-08 14:46:19 +0400

    fix some code

--- src/layouts/ModalItemLayout/ui/ModalItemLayout.jsx
+++ src/layouts/ModalItemLayout/ui/ModalItemLayout.jsx
@@ -2,25 +2,25 @@ import { Button } from "@/ui/Button";
 import cls from "./ModalItemLayout.module.scss";
 
 const ModalItemLayout = (props) => {
-  const { params, options } = props;
+  const { params, price,  options } = props;
   return (
-    <div className={cls.body}>
-      <img className={cls.img} src={params.img} alt="" />
+      <div className={cls.body}>
+        <img className={cls.img} src={params.img} alt="" />
 
-      <div className={cls.content}>
-        <h3 className={cls.title}>{params.title}</h3>
+        <div className={cls.content}>
+          <h3 className={cls.title}>{params.title}</h3>
 
-        {options && options}
+          {options && options}
 
-        <div className={cls.footer}>
-          <span className={cls.price}>Cəmi: {params.price} AZN</span>
+          <div className={cls.footer}>
+            <span className={cls.price}>Cəmi: { price} AZN</span>
 
-          <Button border className={cls.button}>
-            Əlavə et
-          </Button>
+            <Button border className={cls.button}>
+              Əlavə et
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
